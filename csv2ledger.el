c1fb4a3255f29678f4b553a0633e6f3885f84f53
Joost Kremers <joostkremers@fastmail.fm>
2022-10-17 23:30:58 +0200

    Reconcile commits 6023fa3 and 1e4bd7c.

--- csv2ledger.el
+++ csv2ledger.el
@@ -100,7 +100,7 @@ instead of the payee as the title of the entry."
   :type 'string
   :group 'csv2ledger)
 
-(defcustom c2l-csv-columns '(date valuation type description sender payee amount _)
+(defcustom c2l-csv-columns '(date valuation description sender payee amount)
   "List of columns in the CSV file.
 The data in the CSV file is extracted based on this list.  The
 order of elements in the list should therefore represent the
