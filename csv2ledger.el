91a9c504a719cbfc49436c33b985e06e30fce9c1
Joost Kremers <joostkremers@fastmail.com>
2024-06-02 21:21:21 +0200

    Update version number

--- csv2ledger.el
+++ csv2ledger.el
@@ -6,7 +6,7 @@
 ;; Author: Joost Kremers <joostkremers@fastmail.fm>
 ;; Maintainer: Joost Kremers <joostkremers@fastmail.fm>
 ;; Created: 2022
-;; Version: 1.5.0
+;; Version: 1.5.1
 ;; Package-Requires: ((emacs "29.1") ((csv-mode "1.24")))
 ;; URL: https://codeberg.org/joostkremers/csv2ledger
 
